08c74d93aef70b772bc2139571b40efe53605e59
reme2.718 <103392516+rem2718@users.noreply.github.com>
2022-12-07 02:24:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,79 @@
 from Arithmetic import Arithmetic
-from Huffman import Huffman 
+from Huffman import Huffman
+from LZW import LZW
+import time
+import random 
 
-freq_dict = dict()
-print('Hello! How are you today?\n---------------------------------')
+# Alphabets: 
+alphabet0 = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '"', '#', '$', 
+             '%', '&', '(', ')', '*', '+', ',', '-', '.', ':', ';', '<', '=', '>', 
+             '?', '@', '[', ']', '^', '_', '{', '|', '}', '~']
 
-while(True):
-    al = input('What algorithm you wanna try?\nH -> Huffman\nAH -> Adaptive Huffman\nA -> Arithmetic\nE -> Exit\n')
-    match al:
-        case 'H': # Huffman coding
-            freq = input('Enter your frequency table:\n').split(' ') 
-            for e in freq: 
-                e = e.split(':') 
-                freq_dict[e[0]] = int(e[1])
-            p = Huffman(freq_dict)
-            print('Here\'s your Huffman codewords:')
-            p.print_codewords()
-            while True:
-                msg = input('Enter your message:\n')
-                print(f"The encoded message: {p.huffman_encode(msg)}") 
-                choice = input('Wanna encode another message?') 
-                if choice in ('No', 'no', 'n', 'N'): break
-        case 'AH': # Adaptive Huffman coding
-            pass
-        case 'A': # Arithmetic coding
-            freq = input('Enter your probability table:\n').split(' ')
-            for e in freq: 
-                e = e.split(':') 
-                freq_dict[e[0]] = float(e[1])
-            p = Arithmetic(freq_dict)
-            while True:
-                msg = input('Enter your message:\n')
-                en_msg = p.arithmetic_encode(msg)
-                de_msg = p.arithmetic_decode(en_msg, len(msg))
-                print(f"The encoded message: {en_msg}") 
-                print(f"The decoded message: {de_msg}")
-                choice = input('Wanna encode another message?') 
-                if choice in ('No', 'no', 'n', 'N'): break
-        case 'E': # Exit
-            print('Bye!---------------------------------')
-            break
-        case other:
-            print('No match found! try again')
+alphabet1 = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+             'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+             'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
+             'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+             ]
+alphabet2 = alphabet0 + alphabet1
+alphabets = [alphabet0, alphabet1, alphabet2]
+# Message lengths
+length = [500, 1000, 5000]
+
+# Lists and dictionaries for frequency and probability tables
+f_lists = []
+p_lists = []
+f_tables = []
+p_tables = []
+
+for alphabet in alphabets:
+    temp_list = random.sample(range(1, 101), len(alphabet))
+    f_lists.append(temp_list) 
+    p_lists.append([i/sum(temp_list) for i in temp_list])
+
+    f_tables.append(dict(zip(alphabet, temp_list)))
+    p_tables.append(dict(zip(alphabet, p_lists[len(p_lists)-1])))
+
+# Generating 3 random strings based on the alphabets and frequencies above
+print('Generating 3 random strings to input.txt')
+file = open('input.txt', 'w')
+for i in range(3):
+    chars = random.choices(alphabets[i], weights = f_lists[i], k = length[i])
+    msg = ''.join(chars)
+    file.write(msg + '\n')
+file.close()
+# Reading the file again
+print('Reading input.txt')   
+file = open('input.txt', 'r')
+
+# Apply all algorithms for the 3 messages
+j = 0
+for msg in file:
+    print('----------------------------------')
+    msg = msg[:-1]
+    start = time.time()
+    p1 = LZW(alphabets[j]) 
+    encoded = p1.LZW_encode(msg)
+    end = time.time()
+    en_time = round(end - start, 10)
+    decoded = p1.LZW_decode(encoded)
+    print(f"LZW encoding msg {j+1} in: {en_time} secs")
+    print('correct!') if msg == decoded else print('wrong!')
+
+    start = time.time()
+    p2 = Huffman(f_tables[j])
+    encoded = p2.huffman_encode(msg)
+    end = time.time()
+    en_time = round(end - start, 10)
+    decoded = p2.huffman_decode(encoded)
+    print(f"Huffman encoding msg {j+1} in: {en_time} secs")
+    print('correct!') if msg == decoded else print('wrong!')
     
-    print('---------------------------------')   
-    freq_dict.clear()   
-        
+    start = time.time()
+    p3 = Arithmetic(p_tables[j])
+    encoded = p3.arithmetic_encode(msg)
+    end = time.time()
+    en_time = round(end - start, 10)
+    decoded = p3.arithmetic_decode(encoded, len(msg))
+    print(f"Arithmetic encoding msg {j+1} in: {en_time} secs")
+    print('correct!') if msg == decoded else print('wrong!')
+    j += 1
